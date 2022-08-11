11ef11475d79645f765b1786dd824ba9daba6d4e
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2022-08-11 16:31:54 +0000

    Fix launch file

--- src/handle.py
+++ src/handle.py
@@ -1,37 +1,37 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-import rospy
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
+import rospy
 
 
 class CameraHandleConfig:
 
     def __init__(self):
-        self.source_topics: list = rospy.get_param(
-            param_name="~source_topics",
+        self.source_topics = rospy.get_param(
+            param_name="~/camera_handle/source_topics",
             default=[],
         )
-        rospy.logdebug("source topics: %s", self.source_topics)
+        rospy.loginfo("source topics: %s" % self.source_topics)
         assert len(self.source_topics) > 0, \
             "source_topics must not be an empty list"
 
         self.initial_topic = rospy.get_param(
-            param_name="~initial_topic",
+            param_name="~/camera_handle/initial_topic",
             default=self.source_topics[0],
         )
-        rospy.logdebug("initial topic: %s", self.initial_topic)
+        rospy.loginfo("initial topic: %s", self.initial_topic)
 
         self.output_topic = rospy.get_param(
-            param_name="~output_topic",
+            param_name="~/camera_handle/output_topic",
             default="/camera/image_raw",
         )
-        rospy.logdebug("output topic: %s", self.source_topics)
+        rospy.loginfo("output topic: %s", self.output_topic)
 
 
 class CameraHandle:
 
-    def __init__(self, config: CameraHandleConfig = CameraHandleConfig()):
+    def __init__(self, config=CameraHandleConfig()):
         self.config = config
         self.selected_topic = config.initial_topic
 
@@ -64,14 +64,14 @@ class CameraHandle:
             )
 
     def on_handle_updated(self, selected_topic):
-        rospy.logdebug("topic selected: %s", selected_topic)
+        rospy.loginfo("topic selected: %s", selected_topic)
         self.selected_topic = selected_topic
 
-    def on_frame_received(self, image: Image, topic_name: str):
+    def on_frame_received(self, image, topic_name):
         if topic_name == self.selected_topic:
             if not rospy.is_shutdown():
-                rospy.logdebug("republish frame from topic %s to topic %s",
-                               self.selected_topic, self.config.output_topic)
+                rospy.loginfo("republish frame from topic %s to topic %s",
+                              self.selected_topic, self.config.output_topic)
                 self.frames_publisher.publish(image)
 
 
